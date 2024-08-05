00618908d13b3dd18fa919c5385a181605ff4527
Najini Math <najinimath@hotmail.com>
2024-08-05 18:49:01 +0200

    style folder and global css

---
+++ styles/global.js
@@ -0,0 +1,19 @@
+const css = {
+  backgroundColorOne: '#fbdd74',
+  backgroundColorTwo: '#abd1c6',
+  activeIconColor: '#ffff',
+  iconColor: '#6246ea',
+  activeButtonColor: '#004643',
+  gradientColor: linear-gradient(45deg, #f9bc60, #6246ea);
+  fontSizeOne: 96,
+  fontSizeTwo: 60,
+  fontSizeThree: 48,
+  fontSizeFour: 34,
+  fontSizeFive: 24,
+  fontSizeSix: 20,
+  fontFamilyOne: 'Fira Sans',
+  fontFamilyTwo: 'Varela',
+  fontFamilyThree: 'Varela Round',
+};
+
+export default css;
