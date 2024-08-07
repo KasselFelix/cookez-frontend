e54c6bdf564c260e538e5a41ac5efac64d9cdf04
KasselFelix <w-kassel.f@outlook.fr>
2024-08-07 12:34:07 +0200

    add data recipes ingredients

---
+++ modules/ingredients.js
@@ -0,0 +1,136 @@
+const ingredients = [
+    {
+        "name": "Porc",
+        "image": "porc.jpg",
+        "quantity": 100,
+        "nutrition": {"vitamine_A": 0, "vitamine_B": 0.26, "glycemic_index": 0, "calories": 48.4}
+    },
+    {
+        "name": "Ananas",
+        "image": "ananas.jpg",
+        "quantity": 100,
+        "nutrition": {"vitamine_A": 3, "vitamine_B": 0.1, "glycemic_index": 66, "calories": 50}
+    },
+    {
+        "name": "Haricots noirs",
+        "image": "haricots_noirs.jpg",
+        "quantity": 100,
+        "nutrition": {"vitamine_A": 0, "vitamine_B": 0.1, "glycemic_index": 30, "calories": 170}
+    },
+    {
+        "name": "Riz",
+        "image": "riz.jpg",
+        "quantity": 100,
+        "nutrition": {"vitamine_A": 0, "vitamine_B": 0.05, "glycemic_index": 73, "calories": 130}
+    },
+    {
+        "name": "Plantains",
+        "image": "plantains.jpg",
+        "quantity": 100,
+        "nutrition": {"vitamine_A": 0, "vitamine_B": 0.07, "glycemic_index": 55, "calories": 120}
+    },
+    {
+        "name": "Persil",
+        "image": "persil.jpg",
+        "quantity": 100,
+        "nutrition": {"vitamine_A": 421, "vitamine_B": 0.2, "glycemic_index": 0, "calories": 36}
+    },
+    {
+        "name": "Ail",
+        "image": "ail.jpg",
+        "quantity": 100,
+        "nutrition": {"vitamine_A": 0, "vitamine_B": 0.2, "glycemic_index": 0, "calories": 4}
+    },
+    {
+        "name": "Bœuf effiloché",
+        "image": "boeuf_effiloche.jpg",
+        "quantity": 100,
+        "nutrition": {"vitamine_A": 0, "vitamine_B": 0.08, "glycemic_index": 0, "calories": 50}
+    },
+    {
+        "name": "Tomates",
+        "image": "tomates.jpg",
+        "quantity": 100,
+        "nutrition": {"vitamine_A": 42, "vitamine_B": 0.02, "glycemic_index": 15, "calories": 18}
+    },
+    {
+        "name": "Maïs",
+        "image": "mais.jpg",
+        "quantity": 100,
+        "nutrition": {"vitamine_A": 214, "vitamine_B": 0.05, "glycemic_index": 52, "calories": 86}
+    },
+    {
+        "name": "Fromage",
+        "image": "fromage.jpg",
+        "quantity": 100,
+        "nutrition": {"vitamine_A": 300, "vitamine_B": 0.4, "glycemic_index": 0, "calories": 400}
+    },
+    {
+        "name": "Maïs hominy",
+        "image": "mais_hominy.jpg",
+        "quantity": 100,
+        "nutrition": {"vitamine_A": 0, "vitamine_B": 0.07, "glycemic_index": 70, "calories": 119}
+    },
+    {
+        "name": "Poulet",
+        "image": "poulet.jpg",
+        "quantity": 100,
+        "nutrition": {"vitamine_A": 0, "vitamine_B": 0.22, "glycemic_index": 0, "calories": 239}
+    },
+    {
+        "name": "Farine de maïs",
+        "image": "farine_de_mais.jpg",
+        "quantity": 100,
+        "nutrition": {"vitamine_A": 0, "vitamine_B": 0.25, "glycemic_index": 70, "calories": 360}
+    },
+    {
+        "name": "Bœuf",
+        "image": "boeuf.jpg",
+        "quantity": 100,
+        "nutrition": {"vitamine_A": 0, "vitamine_B": 0.08, "glycemic_index": 0, "calories": 50}
+    },
+    {
+        "name": "Sel",
+        "image": "sel.jpg",
+        "quantity": 100,
+        "nutrition": {"vitamine_A": 0, "vitamine_B": 0, "glycemic_index": 0, "calories": 0}
+    },
+    {
+        "name": "Pommes de terre",
+        "image": "pommes_de_terre.jpg",
+        "quantity": 100,
+        "nutrition": {"vitamine_A": 0, "vitamine_B": 0.03, "glycemic_index": 78, "calories": 77}
+    },
+    {
+        "name": "Patates douces",
+        "image": "patates_douces.jpg",
+        "quantity": 100,
+        "nutrition": {"vitamine_A": 19218, "vitamine_B": 0.1, "glycemic_index": 70, "calories": 86}
+    },
+    {
+        "name": "Farine",
+        "image": "farine.jpg",
+        "quantity": 100,
+        "nutrition": {"vitamine_A": 0, "vitamine_B": 0.2, "glycemic_index": 70, "calories": 364}
+    },
+    {
+        "name": "Plantains verts",
+        "image": "plantains_verts.jpg",
+        "quantity": 100,
+        "nutrition": {"vitamine_A": 0, "vitamine_B": 0.07, "glycemic_index": 55, "calories": 120}
+    },
+    {
+        "name": "Farine de tapioca",
+        "image": "farine_de_tapioca.jpg",
+        "quantity": 100,
+        "nutrition": {"vitamine_A": 0, "vitamine_B": 0.2, "glycemic_index": 85, "calories": 358}
+    },
+    {
+        "name": "Chapelure",
+        "image": "chapelure.jpg",
+        "quantity": 100,
+        "nutrition": {"vitamine_A": 0, "vitamine_B": 0.1, "glycemic_index": 70, "calories": 364}
+    }
+]
+
+export default ingredients;
