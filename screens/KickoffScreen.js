9b0535300436fb9770c9668d9ac7355b0bbc1755
Andres <andresangulo90@gmail.com>
2024-08-10 13:15:09 +0200

    kickoff & result screens in progress

--- screens/KickoffScreen.js
+++ screens/KickoffScreen.js
@@ -1,38 +1,32 @@
 import React, { useState, useEffect, useRef } from "react";
-import {
-  SafeAreaView,
-  StyleSheet,
-  TouchableOpacity,
-  ScrollView,
-  View,
-  Image,
-} from "react-native";
+import { SafeAreaView, StyleSheet, TouchableOpacity, ScrollView, View, Image, Text, Modal, TextInput} from "react-native";
 import { Camera, CameraType, FlashMode } from "expo-camera/legacy";
-import { useDispatch,useSelector } from "react-redux";
-import { addIngredientToStore,removeIngredientToStore } from "../reducers/ingredient";
+import { useDispatch, useSelector } from "react-redux";
+import { addIngredientToStore, removeIngredientToStore } from "../reducers/ingredient";
 import FontAwesome from "react-native-vector-icons/FontAwesome";
 import { useIsFocused } from "@react-navigation/native";
 
+import MySmallButton from '../modules/MySmallButton'
 import MyButton from '../modules/MyButton';
-import MySmallButton from "../modules/MyButton";
 import buttonStyles from '../styles/Button';
 import css from "../styles/Global";
 
 
 export default function KickoffScreen({navigation}) {
 	const saveMoney=false;
-	const API_KEY="qD5sB6nb.BaYym0RxgqgmXQb5GEGgUZJshGNQhYby"
+	const API_KEY="RLHrFgPo.2X82YIPeHdDihfr4QvianSMMCg1aX4Hi"
 
   const [hasPermission, setHasPermission] = useState(false);
   const [type, setType] = useState(CameraType.back);
   const [flashMode, setFlashMode] = useState(FlashMode.off);
   const [pictures, setPictures] = useState([]);
+  const [modalVisible, setModalVisible] = useState(false);
 
   const dispatch = useDispatch();
   const isFocused = useIsFocused();
 
   let cameraRef = useRef(null);
-
+  const background = [];
   useEffect(() => {
     (async () => {
       const result = await Camera.requestCameraPermissionsAsync();
@@ -102,7 +96,7 @@ export default function KickoffScreen({navigation}) {
 				name: 'photo.jpg',
 				type: 'image/jpeg',
 			});
-			fetch('http://192.168.100.20:3000/upload', {
+			fetch('http://192.168.1.3:3000/upload', {
 				method: 'POST',
 				body: formData,
 			   })
@@ -139,56 +133,94 @@ export default function KickoffScreen({navigation}) {
 		  </View>
 		);
 	  });
+
+	  const backgroundIngredient = () => {
+		for (let i = pictures.length; i < 3; i++) {
+				background.push(<View key={i} style={sytle=styles.addPicturesContainer}>
+					<FontAwesome name="camera-retro" size={70} color="rgba(255,255,255, 0.4)" />
+					<Text style={styles.text}>Ingrédient</Text>
+				</View>);	
+				
+		}	
+		return  background;
+	  }
 	
 
   return (
-	<SafeAreaView style={styles.container} >
-		<Camera type={type} flashMode={flashMode} ref={(ref) => (cameraRef = ref)} style={styles.camera}>
-				<View style={styles.buttonsCameraContainer}>
-					<TouchableOpacity onPress={() => setType(type === CameraType.back ? CameraType.front : CameraType.back)} style={styles.buttonsCamera}>
-						<FontAwesome name="rotate-right" size={25} color="#ffffff" />
-					</TouchableOpacity>
+	// <ScrollView  contentContainerStyle={styles.scrollView} automaticallyAdjustKeyboardInsets={true}>
+		<SafeAreaView style={styles.container} >
+			<Camera type={type} flashMode={flashMode} ref={(ref) => (cameraRef = ref)} style={styles.camera}>
+					<View style={styles.buttonsCameraContainer}>
+						<TouchableOpacity onPress={() => setType(type === CameraType.back ? CameraType.front : CameraType.back)} style={styles.buttonsCamera}>
+							<FontAwesome name="rotate-right" size={25} color="#ffffff" />
+						</TouchableOpacity>
 
-					<TouchableOpacity onPress={() => setFlashMode(flashMode === FlashMode.off ? FlashMode.torch : FlashMode.off)} style={styles.buttonsCamera}>
-						<FontAwesome name="flash" size={25} color={flashMode === FlashMode.off ? "#ffffff" : "#e8be4b"} />
-					</TouchableOpacity>
+						<TouchableOpacity onPress={() => setFlashMode(flashMode === FlashMode.off ? FlashMode.torch : FlashMode.off)} style={styles.buttonsCamera}>
+							<FontAwesome name="flash" size={25} color={flashMode === FlashMode.off ? "#ffffff" : "#e8be4b"} />
+						</TouchableOpacity>
+					</View>
+			</Camera>
+
+        	<ScrollView horizontal  contentContainerStyle={styles.galleryContainer}>
+				{photos}
+				{backgroundIngredient()}
+        	</ScrollView>
+
+			<View style={styles.containerButtonBottom}>
+				<Modal visible={modalVisible} animationtType="fade" transparent>
+       		 		<View style={styles.modal}>
+       		 		    <View style={styles.modalContainer}>
+							<View style={styles.searchInput}>
+       		 		        	<TextInput placeholder='Ingrédient' placeholderTextColor={'grey'}/>
+							</View>
+								{/* <View style={styles.modalButtons}>
+								</View> */}
+							<View style={styles.modalButtons}>
+								<View style={styles.validButton}>
+									<MySmallButton 
+										dataFlow={()=> setModalVisible(false)}
+										text={'Ajouter'}
+										buttonType={buttonStyles.buttonTwo}
+									/>
+								</View>
+								<View style={styles.backRetour}>
+									<MySmallButton 
+										dataFlow={()=> setModalVisible(false)}
+										// textType={}
+										text={'Retour'}
+										buttonType={buttonStyles.buttonTwo}
+									/>
+								</View>
+							</View>
+       		 		    </View>
+       		 		</View>
+       			</Modal> 
+								
+
+				<View style={styles.buttonSearch}>
+					<MyButton
+						dataFlow={()=> setModalVisible(true)}
+						text={"Chercher"}
+        				buttonType={buttonStyles.buttonTwo}
+					/>
 				</View>
 
-				{/* <View style={styles.snapContainer}>
-					<TouchableOpacity onPress={() => cameraRef && takePicture()}>
-						<FontAwesome name="circle-thin" size={95} color="#ffffff" />
-					</TouchableOpacity>
-				</View> */}
-		</Camera>
-		
-        <ScrollView horizontal  contentContainerStyle={styles.galleryContainer}>
-			{photos}
-			{/* <View style={sytle= }></View> */}
-        </ScrollView>
-		
-		<View style={styles.containerButtonBottom}>
-			<View style={styles.buttonSearch}>
-				<MyButton
-					dataFlow={()=>handleBtn()}
-					text={"Search"}
-        			buttonType={buttonStyles.buttonTwo}
-				/>
+				<View style={styles.buttonNext}>
+					<MyButton
+						dataFlow={()=>handleBtn()}
+						text={"Suivant"}
+        				buttonType={buttonStyles.buttonTwo}
+					/>
+				</View>
 			</View>
-			<View style={styles.buttonNext}>
-				<MyButton
-					dataFlow={()=>handleBtn()}
-					text={"Suivant"}
-        			buttonType={buttonStyles.buttonTwo}
-				/>
-			</View>
-		</View>
 
-		<View style={styles.snapContainer}>
-			<TouchableOpacity onPress={() => cameraRef && takePicture()}>
-				<FontAwesome name="circle-thin" size={95} color="#ffffff" />
-			</TouchableOpacity>
-		</View>
-	</SafeAreaView>
+			<View style={styles.snapContainer}>
+				<TouchableOpacity onPress={() => cameraRef && takePicture()}>
+					<FontAwesome name="circle-thin" size={95} color="#ffffff" />
+				</TouchableOpacity>
+			</View>
+		</SafeAreaView>
+	// </ScrollView>
   )
 }
 
@@ -200,10 +232,61 @@ const styles = StyleSheet.create({
 		paddingTop: css.paddingTop,
 	},
 
+	// scrollView: {
+	// 	alignItems: 'center',
+	// 	paddingBottom: 20,
+	// }, 
+
+	modal: {
+		flex: 1,
+		justifyContent: 'center',
+		alignItems: 'center',
+	},
+
+	modalContainer: {
+		flex: 0,
+		justifyContent: 'center',
+		alignItems: 'center',
+		backgroundColor: css.backgroundColorOne,
+		borderRadius: 20,
+		height: 210,
+		width: 150,
+		alignItems: 'center',
+		shadowColor: '#000',
+		shadowOffset: {
+		  width: 0,
+		  height: 2,
+		},
+		shadowOpacity: 0.25,
+		shadowRadius: 4,
+		elevation: 5,
+	},
+
+	modalButtons: {
+	flex: 0, 
+	justifyContent: 'space-between',
+	},
+
+	validButton: {
+	marginBottom: 20,
+	},
+
+	searchInput: {
+	flex: 0,
+	alignItems: 'center',
+	marginBottom: 20,
+	width: '80%',
+	borderWidth: 1,
+	borderColor: 'grey',
+	backgroundColor: 'rgba(255,255,255, 0.6)',
+	},
+
 	camera: {
 		height:'50%',
 		width:'90%',
 		marginBottom:10,
+		borderRadius: 40,
+		overflow: "hidden",
 	},
 
 	buttonsCameraContainer: {
@@ -241,7 +324,24 @@ const styles = StyleSheet.create({
 	photo: {
 		width: 150,
 		height: 150,
-		margin: 5
+		margin: 5,
+	},
+
+	addPicturesContainer: {
+		flex: 0,
+		justifyContent: 'center',
+		alignItems: 'center',
+		width: 150,
+		height: 150,
+		margin: 5,
+		paddingBottom: 10,
+		borderWidth: 2,
+		borderStyle: 'dashed',
+		borderColor: 'rgba(255,255,255, 0.4)',
+	},
+
+	text: {
+		color: 'rgba(255,255,255, 0.4)',
 	},
 
 	containerButtonBottom: {
@@ -250,7 +350,7 @@ const styles = StyleSheet.create({
 		justifyContent:'space-around',
 		width: '100%',
 		height: 50,
-		marginBottom: 25,
+		marginBottom: 15,
 	},
 
 	buttonSearch: {
