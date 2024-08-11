b2591350abf1a4df7e58014f06d20ead7a19753a
Soufiane <sfn.elmansoury@gmail.com>
2024-08-12 01:29:16 +0200

    Go Back button added on MoreFeaturesPage

--- screens/MoreFeaturesScreen.js
+++ screens/MoreFeaturesScreen.js
@@ -1,12 +1,30 @@
 import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import css from "../styles/Global"
+import buttonStyles from "../styles/Button";
+import MyButton from "../modules/MyButton";
+import MySmallButton from "../modules/MySmallButton";
 
-export default function MoreFeaturesScreen() {
+import FontAwesome from "react-native-vector-icons/FontAwesome"
+
+export default function MoreFeaturesScreen({navigation}) {
   return (
-    <View>
-      <Text>moreFeaturesScreen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <MySmallButton
+        dataFlow={() => navigation.goBack()}
+        text={
+        <FontAwesome name="angle-double-left" size={30} color={"white"} />
+        }
+        buttonType={buttonStyles.buttonSmall}
+      />
+    </SafeAreaView>
   )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: css.backgroundColorOne,
+  },
+})
