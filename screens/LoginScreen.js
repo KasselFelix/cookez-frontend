60ce15851ddf0f77c5036a7cab51f380d2ebe6a1
KasselFelix <w-kassel.f@outlook.fr>
2024-08-12 11:46:11 +0200

    resolve confli

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -4,19 +4,23 @@ import { SafeAreaView } from 'react-native-safe-area-context';
 import SignIn from './SignIn';
 import SignUp from './SignUp';
 import MyButton from '../modules/MyButton';
+import MySmallButton from '../modules/MySmallButton';
 import buttonStyles from '../styles/Button';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
 import css from '../styles/Global';
 
 export default function LoginScreen({navigation}) {
    return (
     <SafeAreaView style={styles.container}>
-      <MySmallButton
+      <View style={styles.btn}>
+      <MySmallButton 
           dataFlow={() => navigation.goBack()}
           text={
             <FontAwesome name="angle-double-left" size={30} color={"white"} />
           }
           buttonType={buttonStyles.buttonSmall}
         />
+      </View>
       <View style={styles.buttonContainer}>
         <SignIn navigation={navigation} />
         <SignUp navigation={navigation} />
@@ -41,4 +45,8 @@ container: {
 buttonContainer:{
   marginBottom: 30,
 },
+btn:{
+  width:'100%',
+  flexDirection:'row'
+}
 })
